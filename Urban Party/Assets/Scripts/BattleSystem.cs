c814e4b77001a4841695b9bbb837a0da4aeb1b8b
F4k1ng1t <nicholj7@my.erau.edu>
2024-11-24 16:08:17 -0700

    Update BattleSystem.cs

--- Urban Party/Assets/Scripts/BattleSystem.cs
+++ Urban Party/Assets/Scripts/BattleSystem.cs
@@ -18,21 +18,36 @@ public class BattleSystem : MonoBehaviour
     public Unit Li_Loid;
     public Unit Tsaine;
 
+    public BattleHUD khyeHUD;
+    public BattleHUD Li_LoidHUD;
+    public BattleHUD TsaineHUD;
+
+    public TextMeshProUGUI turnIndicator;
+
     public TextMeshProUGUI flavorText;
     Unit enemyUnit;
+
+    int partyMembersLeft = 3;
     public void Start()
     {
+        Khye.hud = khyeHUD;
+        Li_Loid.hud = Li_LoidHUD;
+        Tsaine.hud = TsaineHUD;
+
+        khyeHUD.SetHUD(Khye);
+        TsaineHUD.SetHUD(Tsaine);
+        Li_LoidHUD.SetHUD(Li_Loid);
+
         state = BattleStates.START;
         StartCoroutine(SetupBattle());
     }
     IEnumerator SetupBattle()
     {
-
         GameObject enemy = CreateEnemy();
         enemyUnit = enemy.GetComponent<Unit>();
         //Connecting enemy to healthbar
         enemyUnit.hud = Instantiate(enemyInfoPrefab, enemyInfoBackground.transform).GetComponent<BattleHUD>();
-        enemyBattleHUD.SetHUD(enemyPrefab.GetComponent<Unit>());
+        enemyBattleHUD.SetHUD(enemyUnit);
 
         flavorText.text = "A battle begins...";
         yield return new WaitForSeconds(2);
@@ -44,22 +59,42 @@ public class BattleSystem : MonoBehaviour
         state = BattleStates.KHYETURN;
         flavorText.text = $"What will Khye do?";
     }
+    void TsaineTurn()
+    {
+        flavorText.text = $"What will Tsaine do?";
+    }
+    void LiLoidd()
+    {
+        flavorText.text = $"What will Li-Loidd do";
+    }
     public void OnAttackButton()
     {
         Debug.Log($"This Code runs, {state}");
         switch (state)
         {
             case BattleStates.KHYETURN:
-                StartCoroutine(PlayerAttack(Khye));
+                if (Khye.currentHP != 0)
+                {
+                    StartCoroutine(PlayerAttack(Khye));
+                }
                 state = BattleStates.TSAINETURN;
+                
                 break;
             
             case BattleStates.TSAINETURN:
-                StartCoroutine(PlayerAttack(Tsaine));
+
+                if (Tsaine.currentHP != 0)
+                {
+                    StartCoroutine(PlayerAttack(Tsaine));
+                }
+                
                 state = BattleStates.LILOIDDTURN;
                 break;
             case BattleStates.LILOIDDTURN:
-                StartCoroutine(PlayerAttack(Li_Loid));
+                if (Li_Loid.currentHP != 0)
+                {
+                    StartCoroutine(PlayerAttack(Li_Loid));
+                }
                 StartCoroutine(EnemyAttack(enemyUnit));
                 state = BattleStates.ENEMYTURN;
                 break;
@@ -76,17 +111,55 @@ public class BattleSystem : MonoBehaviour
         if (isDead)
         {
             Destroy(enemyUnit.gameObject);
+            state = BattleStates.WIN;
         }
     }
     IEnumerator EnemyAttack(Unit unit)
     {
-        int attacked = Random.Range(1, 3);
-
-        bool isDead = 
-        yield return new WaitForSeconds(2);
+        for (int i = 0; i < 3; i++)
+        {
+            int attacked = Random.Range(1, 3);
+            Unit attackedUnit = null;
+            switch (attacked)
+            {
+                case 1:
+                    attackedUnit = Khye;
+                    flavorText.text = $"{unit.name} attacks Khye for {unit.damage} damage!";
+                    break;
+                case 2:
+                    attackedUnit = Tsaine;
+                    flavorText.text = $"{unit.name} attacks Khye for {unit.damage} damage!";
+                    break;
+                case 3:
+                    attackedUnit = Li_Loid;
+                    flavorText.text = $"{unit.name} attacks Khye for {unit.damage} damage!";
+                    break;
+            }
+            Debug.Log("working");
+            bool isDead = attackedUnit.TakeDamage(unit.damage);
+            yield return new WaitForSeconds(2);
+            if (isDead)
+            {
+                partyMembersLeft--;
+                if (partyMembersLeft == 0)
+                {
+                    state = BattleStates.LOSE;
+                }
+            }
+        }
+        state = BattleStates.KHYETURN;
 
     }
-
+    IEnumerator Win()
+    {
+        flavorText.text = $"You win!!!";
+        yield return new WaitForSeconds(2);
+    }
+    IEnumerator Lose()
+    {
+        flavorText.text = $"You lose...";
+        yield return new WaitForSeconds(2);
+    }
 
     public GameObject CreateEnemy()
     {
