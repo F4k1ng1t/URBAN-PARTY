dd854c2d0de1d0ca679e98e91201ea91ee78900a
F4k1ng1t <nicholj7@my.erau.edu>
2024-11-23 18:34:54 -0700

    battle scene

---
+++ Urban Party/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleStates { START, PLAYERTURN, ENEMYTURN, WIN, LOSE }
+public class BattleSystem : MonoBehaviour
+{
+    
+}
